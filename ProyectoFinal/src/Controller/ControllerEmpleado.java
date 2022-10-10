66c9ef0d137d7f63a8f3ee61ef1986171bc9114b
Jose Gramajo <gra22907@uvg.edu.gt>
2022-10-09 21:19:04 -0600

    Controller clases

---
+++ ProyectoFinal/src/Controller/ControllerEmpleado.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author Jose Gramajo
+ */
+public class ControllerEmpleado {
+    public void tabla(String valor){
+        
+    }
+    public void limpiar(){
+        
+    }
+    public void guardar(){
+        
+    }
+    public void consultar(){
+        
+    }
+    public void actualizar(){
+        
+    }
+    public void eliminar(){
+        
+    }
+    public void filtro(){
+        
+    }
+}
