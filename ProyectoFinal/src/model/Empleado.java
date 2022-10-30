7b266ee9f19d99b952099a9f9df18ebd2158b21c
Diego <diegopablo012@gmail.com>
2022-10-30 15:48:39 -0600

    Empelados

---
+++ ProyectoFinal/src/model/Empleado.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author diego
+ */
+public class Empleado {
+    private int id;
+    private String nombre;
+    private int edad;
+    private String numero;
+    private String puesto;
+
+    public Empleado(int id, String nombre, int edad, String numero, String puesto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.numero = numero;
+        this.puesto = puesto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    
+    
+}
