902e52085179ba0be83a60b4d6956900a08de991
Ruth Anaí de León <dele22428@uvg.edu.gt>
2022-10-31 18:22:59 -0600

    Se creo la clase Usuario con sus getters y setters

---
+++ ProyectoFinal/src/model/Usuario.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Usuario {
+    
+    private String user;
+    private String contra;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+    
+    
+            
+    
+}
